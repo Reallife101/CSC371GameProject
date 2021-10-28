47d9fd83b62046627f4d83fdbaba9e03952db638
Ryan Ngoon <ryan.ngoon@gmail.com>
2021-10-27 19:04:00 -0700

    Fix enemy movement ai

--- Assets/Scripts/moveTowardsPlayer.cs
+++ Assets/Scripts/moveTowardsPlayer.cs
@@ -10,17 +10,22 @@ public class moveTowardsPlayer : MonoBehaviour
     public float minDistance = 25f;
     public float maxDistance = 40f;
 
-
+    [SerializeField]
+    int playerLayer = 8;
 
     // Update is called once per frame
     void Update()
     {
         transform.LookAt(player.transform);
+        RaycastHit hit;
 
         float dist = Vector3.Distance(transform.position, player.transform.position);
-        if (dist > minDistance && dist < maxDistance)
+        if (dist > minDistance && dist < maxDistance && Physics.Raycast(transform.position + Vector3.up, transform.forward, out hit, maxDistance))
         {
-            agent.SetDestination(player.transform.position);
+            if (hit.collider.gameObject.layer == playerLayer)
+            {
+                agent.SetDestination(player.transform.position);
+            }
         }
     }
 }
