88ebe3da0e4c60cd5267ea540eed8f676c17a457
Ryan Ngoon <ryan.ngoon@gmail.com>
2021-10-31 00:34:17 -0700

    Add better wave spawner

---
+++ Assets/EnemyWaveManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaveManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject walls;
+    [SerializeField]
+    List<GameObject> waves;
+    [SerializeField]
+    GameObject end;
+
+    private int waveNum = 1;
+    private bool start = false;
+    private bool done = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveNum = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (done)
+        {
+            gameObject.SetActive(false);
+        } else
+        if (waves[waveNum].transform.childCount == 0)
+        {
+            getNext().SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !start)
+        {
+            start = false;
+            walls.SetActive(true);
+            waves[waveNum].SetActive(true);
+        }
+    }
+
+    private GameObject getNext()
+    {
+        waveNum += 1;
+        if (waveNum == waves.Capacity)
+        {
+            walls.SetActive(false);
+            done = true;
+            return end;
+        }
+
+        return waves[waveNum];
+    }
+}
