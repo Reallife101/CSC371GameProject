5adf5e1b3e31d87c18e273a1148e22c70f9daa35
Rye Cassan <26152348+hondarider29@users.noreply.github.com>
2021-10-31 22:00:44 -0700

    UnderClock Ability Added
    Added the scripts and prefab necessary for the UnderClock ability to work

---
+++ Assets/Scripts/Abilities/UnderClockAbility.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderClockAbility : Ability
+{
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] LayerMask wallMask;
+
+    public float Range = 50f;
+    public GameObject effectTrigger;
+
+    // Used to set the intial cooldown
+    private void Awake()
+    {
+        Cooldown = 20f;
+        OnCooldown = false;
+    }
+
+    public override void TriggerEffect(Camera cam)
+    {
+        // Gets the Mouse posistion
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, groundMask) & !OnCooldown)
+        {
+            // Checks if there is a wall in the ray
+            Vector3 targetHitPoint = hit.point;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, wallMask))
+            {
+                targetHitPoint.x = hit.point.x;
+                targetHitPoint.z = hit.point.z;
+            }
+
+            if (InRange(targetHitPoint, transform.position))
+            {
+                // Instantiates the aoe effet
+                Instantiate(effectTrigger,
+                    new Vector3(targetHitPoint.x, targetHitPoint.y + 1f, targetHitPoint.z),
+                    new Quaternion(0f, 0f, 0f, 0f));
+                StartCoroutine(HandleCoolDown());
+            }
+        }
+    }
+    private bool InRange(Vector3 a, Vector3 b)
+    {
+        float distance;
+        distance = Vector3.Distance(new Vector3(a.x, 0f, a.z),
+                                    new Vector3(b.x, 0f, b.z));
+        return distance <= Range;
+    }
+}
