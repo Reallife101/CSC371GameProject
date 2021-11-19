281163d1d8e4bcf72a5ffbf6a2321da1c9a5ee78
Ryan Ngoon <ryan.ngoon@gmail.com>
2021-11-18 21:31:16 -0800

    Begin adding start menu

---
+++ Assets/Scripts/musicManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicManager : MonoBehaviour
+{
+    public bool[] songsInUse;
+    public bool[] songsUnlocked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
