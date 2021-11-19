869bf99e2213818713be6d9f5ce0f552be63bb4e
Ryan Ngoon <ryan.ngoon@gmail.com>
2021-11-18 23:26:51 -0800

    Add Music reward system

--- Assets/Scripts/bgmManager.cs
+++ Assets/Scripts/bgmManager.cs
@@ -14,29 +14,44 @@ public class bgmManager : MonoBehaviour
     private AudioSource au;
     private int trackCounter;
     private float timer;
+    private musicData data;
+    private List<AudioClip> usedClips = new List<AudioClip>();
 
     void Start()
     {
+        data = saveMusic.loadMusic();
+        addSongs();
         au = GetComponent<AudioSource>();
         trackCounter = 0;
-        au.PlayOneShot(acs[trackCounter]);
+        au.PlayOneShot(usedClips[trackCounter]);
         showTrackUI();
+    }
+    
+    void addSongs()
+    {
+        for (int i=0; i< acs.Capacity; i++)
+        {
+            if (data.songsInUse[i])
+            {
+                usedClips.Add(acs[i]);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer >= acs[trackCounter].length)
+        if (timer >= usedClips[trackCounter].length)
         {
             trackCounter += 1;
 
-            if (trackCounter >= acs.Capacity)
+            if (trackCounter >= usedClips.Capacity)
             {
                 trackCounter = 0;
             }
 
             timer = 0;
-            au.PlayOneShot(acs[trackCounter]);
+            au.PlayOneShot(usedClips[trackCounter]);
             showTrackUI();
         }
         timer += Time.deltaTime;
@@ -45,7 +60,7 @@ public class bgmManager : MonoBehaviour
     void showTrackUI()
     {
         mainUI.SetActive(true);
-        trackUI.GetComponent<TMP_Text>().text = "Now Playing: "+ acs[trackCounter].name;
+        trackUI.GetComponent<TMP_Text>().text = "Now Playing: "+ usedClips[trackCounter].name;
         StartCoroutine(hideUI(5f));
     }
 
--- Assets/Scripts/musicManager.cs
+++ Assets/Scripts/musicManager.cs
@@ -1,21 +1,115 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class musicManager : MonoBehaviour
 {
-    public bool[] songsInUse;
-    public bool[] songsUnlocked;
+    public bool[] songsInUse = new bool[10];
+    public bool[] songsUnlocked = new bool[10];
 
-    // Start is called before the first frame update
+    [SerializeField]
+    List<GameObject> songsUI;
+
     void Start()
     {
-        
+        prepCovers();
+    }
+    
+    private void prepCovers()
+    {
+        for (int i = 0; i< 10; i++)
+        {
+            if (songsUnlocked[i])
+            {
+                songsUI[i].SetActive(true);
+            }
+            else
+            {
+                songsUI[i].SetActive(false);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setSong0(bool b)
     {
-        
+        songsInUse[0] = b;
     }
+    public void setSong1(bool b)
+    {
+        songsInUse[1] = b;
+    }
+    public void setSong2(bool b)
+    {
+        songsInUse[2] = b;
+    }
+    public void setSong3(bool b)
+    {
+        songsInUse[3] = b;
+    }
+    public void setSong4(bool b)
+    {
+        songsInUse[4] = b;
+    }
+    public void setSong5(bool b)
+    {
+        songsInUse[5] = b;
+    }
+    public void setSong6(bool b)
+    {
+        songsInUse[6] = b;
+    }
+    public void setSong7(bool b)
+    {
+        songsInUse[7] = b;
+    }
+    public void setSong8(bool b)
+    {
+        songsInUse[8] = b;
+    }
+    public void setSong9(bool b)
+    {
+        songsInUse[9] = b;
+    }
+
+    public void SaveMusic()
+    {
+        saveMusic.SaveMusic(this);
+    }
+
+    public void LoadMusic()
+    {
+        musicData data = saveMusic.loadMusic();
+        if (data != null)
+        {
+            songsInUse = data.songsInUse;
+            songsUnlocked = data.songsUnlocked;
+        }
+        else
+        {
+            songsInUse = new bool[10];
+            songsUnlocked = new bool[10];
+            songsInUse[0] = true;
+            songsUnlocked[0] = true;
+        }
+
+        updateUI();
+    }
+
+    void updateUI()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (songsInUse[i])
+            {
+                songsUI[i].GetComponentInChildren<Toggle>().isOn = true;
+            }
+            else
+            {
+                songsUI[i].GetComponentInChildren<Toggle>().isOn = false;
+            }
+        }
+    }
+
+
 }
