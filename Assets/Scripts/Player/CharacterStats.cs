faf5566bd495feb519036cfb8d84e09d1e8b6298
Rye Cassan <26152348+hondarider29@users.noreply.github.com>
2021-11-20 15:53:55 -0800

    Stats Scriptable Object

---
+++ Assets/Scripts/Player/CharacterStats.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterStats", menuName = "StatsMenu")]
+public class CharacterStats : ScriptableObject
+{
+    // Player Stats
+    [SerializeField] private const int defaultHealthMax = 100; 
+    public int HealthMax;
+
+    [SerializeField] private const int defaultBulletDamage = 5;
+    public int BulletDamage;
+
+
+    // Score & Upgrade Points
+    [SerializeField] private const int defaultUpgradePoints = 0;
+    public int TotalUpgradePoints;
+    public int AvailableUpgradePoints;
+    public int SpentUpgradePointsPlayer;
+    public int SpentUpgradePointsMovement;
+    public int SpentUpgradePointsCC;
+    public int SpentUpgradePointsDamage;
+
+    public int Score;
+
+
+    // Abilty Stats
+
+    // Movement Abilty
+    [SerializeField] private const float defaultMovementCooldown = 30f;
+    public float MovementCooldown;
+
+    [SerializeField] private const float defaultMovementDurration = 10f;
+    public float MovementDurration;
+
+    [SerializeField] private const float defaultMovementRange = 10f;
+    public float MovementRange;
+
+    [SerializeField] private const bool defaultTeleport = false;
+    public bool Teleport;
+
+
+    // CrowdControl Abilitiy
+    [SerializeField] private const float defaultCCAOERadius = 10f;
+    public float CCAOERange;
+
+    [SerializeField] private const float defaultCCDurration = 10f;
+    public float CCDurration;
+
+    [SerializeField] private const bool defaultRoot = false;
+    public bool Root;
+
+
+    // Damage Ability
+    [SerializeField] private const float defaultDamageAOERadius = 10f;
+    public float DamageAOERadius;
+
+    [SerializeField] private const int defaultAbilityDamage = 10;
+    public int AbilityDamage;
+
+    [SerializeField] private const bool defaultFormat = false;
+    public bool Format;
+
+    
+    // Sets Individual Sections back to default states, and refunds UpgradePoints
+    public void ResetPlayer()
+    {
+        HealthMax = defaultHealthMax;
+        BulletDamage = defaultBulletDamage;
+        AvailableUpgradePoints += SpentUpgradePointsPlayer;
+        SpentUpgradePointsPlayer = 0;
+    }
+
+    public void ResetMovementAbility()
+    {
+        MovementCooldown = defaultMovementCooldown;
+        MovementDurration = defaultMovementDurration;
+        MovementRange = defaultMovementRange;
+        Teleport = defaultTeleport;
+        AvailableUpgradePoints += SpentUpgradePointsMovement;
+        SpentUpgradePointsMovement = 0;
+    }
+
+    public void ResetCCAbility()
+    {
+        CCAOERange = defaultCCAOERadius;
+        CCDurration = defaultCCDurration;
+        Root = defaultRoot;
+        AvailableUpgradePoints += SpentUpgradePointsCC;
+        SpentUpgradePointsCC = 0;
+    }
+
+    public void resetDamageAbility()
+    {
+        DamageAOERadius = defaultDamageAOERadius;
+        AbilityDamage = defaultAbilityDamage;
+        Format = defaultFormat;
+        AvailableUpgradePoints += SpentUpgradePointsDamage;
+        SpentUpgradePointsDamage = 0;
+    }
+}
