efb302c09f567e2f86e9b282fd3daafaf3ffc98e
pipfeld <starstruckcountry@gmail.com>
2021-11-28 16:42:01 -0800

    underclock effect instantiates properly now

--- Assets/Scripts/Abilities/Effects/CrowdControlEffect.cs
+++ Assets/Scripts/Abilities/Effects/CrowdControlEffect.cs
@@ -34,7 +34,6 @@ public class CrowdControlEffect : MonoBehaviour
     {
         if (other.CompareTag(targetTag))
         {
-            Debug.Log("Hit");
             StartCoroutine(HandleSlow(other));
         }
     }
