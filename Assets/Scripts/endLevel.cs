d70dd98d7ff3c5fa2ff7b964a7d2060bc4dd72de
Ryan Ngoon <ryan.ngoon@gmail.com>
2021-11-29 18:52:34 -0800

    Start rewards screen

--- Assets/Scripts/endLevel.cs
+++ Assets/Scripts/endLevel.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class endLevel : MonoBehaviour
 {
     public GameObject winScreen;
+
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Player")
         {
             winScreen.SetActive(true);
             gameObject.SetActive(false);
+            player.SetActive(false);
         }
     }
 }
